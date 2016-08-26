b772158eb03986b9a869e671f7d74935bda95266
Kamil Lewan <carmel4a@o2.pl>
2016-08-26 18:16:22 +0200

    0.0.0.2 First attempt to GUI
    A single plane attached to botton of screen, wich is responsive.

---
+++ scenes/Game/Game.gd
@@ -0,0 +1,16 @@
+####################################################
+#                                                  #
+# Main "in game" scene.                            #
+# Here will be everything wich hppens in gameplay. #
+#                                                  #
+####################################################
+
+extends Node
+
+
+func _ready():
+	# Called every time the node is added to the scene.
+	# Initialization here
+	pass
+
+
---
+++ scenes/User_interface/User_interface.gd
@@ -0,0 +1,14 @@
+
+extends CanvasLayer
+
+# Type of action wich user may do, when use right mouse button. 
+var type_of_action = 0
+# Action wich was chosen. //not shure if it'll be necessary
+var chosen_action = 0
+
+func _ready():
+	# Called every time the node is added to the scene.
+	# Initialization here
+	pass
+
+
