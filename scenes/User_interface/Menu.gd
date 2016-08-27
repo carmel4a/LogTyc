74b35da3e77633361265ea5ce7981b697eef3cc2
Kamil Lewan <carmel4a@o2.pl>
2016-08-28 00:43:33 +0200

    0.0.0.5 Fix context menu
    No more bug with RMB and dragging screen.
    Added globals for menu.

--- scenes/User_interface/Menu.gd
+++ scenes/User_interface/Menu.gd
@@ -1,18 +1,48 @@
 
 extends PopupMenu
-
+# time to move a mouse
+var timer_delay = 0.2
+# magrin in px between ckick and drag
+var click_margin = 20
+# --------------------------
+# Is menu showed?
 var showed = 0
+# Vector of mouse's motion.
+var shift = Vector2(0,0)
+# If button was preesed?
+# idle 0
+# pressed 1
+# drag 2
+# relased 3
+var pressd = 0
 
 func _ready():
 	set_process(true)
+	set_process_input(true)
 
 func _process(delta):
-	if get_parent().type_of_action == 0 and Input.is_mouse_button_pressed(BUTTON_RIGHT) and showed !=1:
-		clear()
-		add_item("test", 0, 0)
-		set_pos(get_viewport().get_mouse_pos())
-		show()
-		showed = 1
 
-	if is_hidden()==true:
+	if get_parent().type_of_action == 0 and showed ==0 and pressd==1:
+		# reset shift
+		shift = Vector2(0,0)
+		shift = get_viewport().get_mouse_pos()
+		pressd =2
+	
+	if get_parent().type_of_action == 0 and pressd==3:
+
+		if abs(shift.length()-get_viewport().get_mouse_pos().length()) <= click_margin:
+			clear()
+			add_item("test", 0, 0)
+			set_pos(get_viewport().get_mouse_pos())
+			show_modal()
+			showed = 1
+		pressd = 0
+
+	if is_hidden() == true:
 		showed = 0
+
+func _input(event):
+	if event.is_action_pressed("con_menu"):
+		pressd = 1
+	if event.is_action_released("con_menu"):
+		pressd = 3
--- scenes/User_interface/User_interface.gd
+++ scenes/User_interface/User_interface.gd
@@ -1,24 +1,25 @@
+##########################################################
+#                                                        #
+# This is node wich controls main functions of UI, i.e.  #
+# stores globals. Every part of UI should be autonomous. #
+# Members shoul be necessary.                            #
+#                                                        #
+##########################################################
+
 
 extends CanvasLayer
 
 # Type of action wich user may do, when use right mouse button. 
 var type_of_action = 0
-# Action wich was chosen. //not shure if it'll be necessary
-var chosen_action = 0
 
 func _ready():
-	# Called every time the node is added to the scene.
-	# Initialization here
 	pass
 
-
-
-
+# Managing signals
+# Singals fron botton buttons
 func _on_Bulid_btt_pressed():
 	type_of_action = 0
-
 func _on_Orders_btt_pressed():
 	type_of_action = 1
-
 func _on_Stats_btt_pressed():
 	type_of_action = 2
